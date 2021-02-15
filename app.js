c4194babe50528417d22cb7f8f3d4baef2406565
Junaid S <intelpik@gmail.com>
2021-02-15 11:59:11 +0530

    project finshed

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ gsap.utils.toArray('.section').forEach(section => {
   ScrollTrigger.create({
     trigger: section,
     start: 'top top',
+    duration: 5,
     pin: true,
+  
     pinSpacing: false
   });
 });
+
